999c54beedd567241970e7f15654b219f8aa5cff
ScabbardaZup <117903671+ScabbardaZup@users.noreply.github.com>
2024-04-25 15:53:12 +0000

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+//import './components/Header/header stule.css';
+//import './App.css';
+import Header from './components/Header/header.js'
+
+function App() {
+  return (
+    <div className="App">
+    <Header />  
+      
+    </div>
+  );
+}
+
+export default App;
